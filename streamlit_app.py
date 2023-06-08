4ab7df47d0392282bdb91b4b7660c6919f54d641
Soyeon <annkwon1123@gmail.com>
2023-06-08 13:35:22 +0900

    streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,15 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 
 st.write(""" 
 #BOOL-BIRD
 accident-data.
 """)
 
-window = st.slider("공정률")
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+st.text_input("Your name", key="name")
+
+# You can access the value at any point with:
+st.session_state.name
