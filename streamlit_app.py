640c5a2c442dcc515efdd5f529a4a3b9a095fe4e
Soyeon <annkwon1123@gmail.com>
2023-06-08 13:43:45 +0900

    streamlit2

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,11 @@ st.write("""
 accident-data.
 """)
 
+df = pd.read_csv('./output.csv')
 x = st.slider('x')  # 👈 this is a widget
+y = df.cloumns
 st.write(x, 'squared is', x * x)
+st.write(y)
 st.text_input("Your name", key="name")
 
 # You can access the value at any point with:
